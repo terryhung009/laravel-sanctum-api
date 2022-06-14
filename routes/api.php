4674f14ed4315b346c4e2cc9244d6cd8f4f17947
danny <khc741213@gmail.com>
2022-06-14 16:16:31 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,8 +22,20 @@ use App\Http\Controllers\ProductController;
 //    return $request->user();
 //});
 
-Route::resource('products',ProductController::class);
-Route::get('products/search/{name}', [ProductController::class,'search']);
+//Public routes
+//Route::resource('products',ProductController::class);
+Route::get('/products', [ProductController::class,'index']);
+Route::post('/register',[AuthController::class,'register']);
+Route::get('/products/{id}', [ProductController::class,'show']);
+Route::get('/products/search/{name}', [ProductController::class,'search']);
+
+//Protected routes
+Route::group(['middleware'=>['auth:sanctum']] , function () {
+//    Route::get('products/search/{name}', [ProductController::class,'search']);
+    Route::post('products',[ProductController::class,'store']);
+    Route::put('products/{id}',[ProductController::class,'update']);
+    Route::delete('products/{id}',[ProductController::class,'destroy']);
+});
 
 //Route::get('products', [ProductController::class,'index']);
 //
